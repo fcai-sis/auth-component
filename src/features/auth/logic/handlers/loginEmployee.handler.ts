9dd8e5a11731254c59451c64a8813aed6f048ab5
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-04-20 14:57:42 +0200

    Add handlers for login of students and employees

---
+++ src/features/auth/logic/handlers/loginEmployee.handler.ts
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+import { Request, Response } from 'express';
+import { Role } from '@fcai-sis/shared-middlewares';
+import { EmployeeModel } from '@fcai-sis/shared-models';
+
+const loginEmployeeHandler = async (req: Request, res: Response) => {
+  try {
+    const { username, password } = req.body;
+
+    // Find user by email
+    // TODO: Use password
+    const employee = await EmployeeModel.findOne({ username });
+
+    if (!employee) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    // Generate JWT token with user's id and role
+    const token = jwt.sign({
+      userId: employee.userId.toString(),
+      role: Role.EMPLOYEE
+    }, process.env.JWT_SECRET as string);
+
+    res.json({ token });
+  } catch (error) {
+    console.error('User login failed:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+export default loginEmployeeHandler;
---
+++ src/features/auth/logic/handlers/loginStudent.handler.ts
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+import { Request, Response } from 'express';
+import { Role } from '@fcai-sis/shared-middlewares';
+import { StudentModel } from '@fcai-sis/shared-models';
+
+const loginStudentHandler = async (req: Request, res: Response) => {
+  try {
+    const { studentId, password } = req.body;
+
+    // Find user by email
+    // TODO: Use password
+    const student = await StudentModel.findOne({ studentId });
+
+    if (!student) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    // Generate JWT token with user's id and role
+    const token = jwt.sign({
+      userId: student.userId.toString(),
+      role: Role.STUDENT,
+    }, process.env.JWT_SECRET as string);
+
+    res.json({ token });
+  } catch (error) {
+    console.error('User login failed:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+export default loginStudentHandler;
