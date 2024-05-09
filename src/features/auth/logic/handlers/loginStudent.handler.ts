b52b98e045d31808a1053f9a000547b43765791e
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-05-09 14:41:45 +0300

    Add password validation in student

--- src/features/auth/logic/handlers/loginStudent.handler.ts
+++ src/features/auth/logic/handlers/loginStudent.handler.ts
@@ -1,17 +1,28 @@
+import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { Request, Response } from 'express';
 import { Role } from '@fcai-sis/shared-middlewares';
-import { StudentModel } from '@fcai-sis/shared-models';
+import { StudentModel, UserModel } from '@fcai-sis/shared-models';
 
 const loginStudentHandler = async (req: Request, res: Response) => {
   try {
     const { studentId, password } = req.body;
 
-    // Find user by email
-    // TODO: Use password
     const student = await StudentModel.findOne({ studentId });
 
     if (!student) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    const user = await UserModel.findById(student.userId);
+
+    if (!user) {
+      return res.status(500).json({ message: 'User not found' });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatch) {
       return res.status(401).json({ message: 'Invalid credentials' });
     }
 
