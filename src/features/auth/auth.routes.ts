bbf5483cc471599f97d6418a38bca130c4c4b8bb
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-07-05 13:17:24 +0300

    chore: Update npm dependencies to latest versions

--- src/features/auth/auth.routes.ts
+++ src/features/auth/auth.routes.ts
@@ -4,9 +4,13 @@ import { asyncHandler } from "@fcai-sis/shared-utilities";
 import loginStudentHandler from "./logic/handlers/loginStudent.handler";
 import loginEmployeeHandler from "./logic/handlers/loginEmployee.handler";
 import loginAdminHandler from "./logic/handlers/loginAdmin.handler";
+import loginInstructorHandler from "./logic/handlers/loginInstructor.handler";
+import loginTaHandler from "./logic/handlers/loginTa.handler";
 
 export default (router: Router) => {
   router.post("/student", asyncHandler(loginStudentHandler));
   router.post("/employee", asyncHandler(loginEmployeeHandler));
   router.post("/admin", asyncHandler(loginAdminHandler));
+  router.post("/instructor", asyncHandler(loginInstructorHandler));
+  router.post("/ta", asyncHandler(loginTaHandler));
 };
---
+++ src/features/auth/logic/handlers/loginTa.handler.ts
@@ -0,0 +1,45 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { Request, Response } from "express";
+import { Role } from "@fcai-sis/shared-middlewares";
+import { TeachingAssistantModel, UserModel } from "@fcai-sis/shared-models";
+
+const loginTaHandler = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    const instructor = await TeachingAssistantModel.findOne({ email });
+
+    if (!instructor) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const user = await UserModel.findById(instructor.user);
+
+    if (!user) {
+      return res.status(500).json({ message: "wtf? user not found" });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    // Generate JWT token with user's id and role
+    const token = jwt.sign(
+      {
+        userId: instructor.user.toString(),
+        role: Role.TEACHING_ASSISTANT,
+      },
+      process.env.JWT_SECRET as string
+    );
+
+    res.json({ token });
+  } catch (error) {
+    console.error("User login failed:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export default loginTaHandler;
